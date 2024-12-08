a5fcb283f6b868911cbd98d28964f12955e0c4e6
Valentin GUILLON (home) <valentin.guillon93430@gmail.com>
2024-12-08 00:55:14 +0000

    variable typing

--- client.py
+++ client.py
@@ -276,7 +276,7 @@ class ClientUi(tk.Tk):
         self.network_client.leaveGroup(groupName)
 
 
-    def try_create_group(self, groupName):
+    def try_create_group(self, groupName:str):
         print(f'Try to create the groupe "{groupName}"')
         # refuse empty name
         if not (groupName and groupName.lstrip()):
